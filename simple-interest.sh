9ed21038892752bccbeafd18b6fe282bdb4f3d77
User-1008 <167282449+User-1008@users.noreply.github.com>
2025-07-04 14:57:29 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+# Calculate simple interest
+echo "Enter principal amount:"
+read p
+echo "Enter rate of interest:"
+read r
+echo "Enter time (years):"
+read t
+interest=$(( (p * r * t) / 100 ))
+echo "Simple Interest: $interest"
